684615c9bf3336c3c0b1a4ecf95123604858a7e2
crivmar <carlosrivero1988@gmail.com>
2021-03-05 19:24:30 +0100

    Añadida función menú y llamada en programa.

--- funciones.py
+++ funciones.py
@@ -4,5 +4,21 @@ def leer_documento(d):
         l=json.load(f)
     return l
 
+def menu():
+    def menu():
+    print('''
+    1. Nombre e imagenes de los planetas del Sistema Solar.
+    2. Nº de satélites por planeta.
+    3. Satélites de un planeta.
+    4. Búsqueda de planeta por su satélite.
+    5. Distancia de los planetas al Sol.
+    6. Salir
+    ''')
+    opcion=int(input("Elige una opción: "))
+    print("-"*40)
+    while opcion<1 or opcion>6:
+        print("Error")
+        opcion=int(input("Elige una opción: "))
+    return opcion
 
 
--- programa.py
+++ programa.py
@@ -3,3 +3,8 @@ from funciones import *
 documento=leer_documento("/home/crivero/Descargas/Git/JSON-planetas/planets.json")
 
 print(documento)
+
+m=0
+while m!=6:
+    m=menu()
+    ## Ejercicio 1##
